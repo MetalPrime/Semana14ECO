f86ca364ac5f4eb6818260235861b8a88984ab90
Jhon Garcia <metalprime57@gmail.com>
2020-11-18 14:33:22 -0500

    Create firebase.js

---
+++ script/firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAJAb5xt0drUeCa2dBKMhHf3z8QMaHI2y4",
+    authDomain: "s10project.firebaseapp.com",
+    databaseURL: "https://s10project.firebaseio.com",
+    projectId: "s10project",
+    storageBucket: "s10project.appspot.com",
+    messagingSenderId: "830069995055",
+    appId: "1:830069995055:web:659945a73e1fbb31dbf8da",
+    measurementId: "G-FRY4D8DF1Y"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
